5cf8f31eb638a137169263540df392662caa4625
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2023-10-17 12:10:14 -0400

    API

---
+++ index.js
@@ -0,0 +1,15 @@
+import express, { response } from "express";
+import { getAllStudents , addStudent } from "./src/students.js";
+
+const app = express()
+app.use(express.json())
+
+app.get("/students",getAllStudents)
+app.post("/students", addStudent)
+
+
+app.listen(3000, () => {
+    console.log('Listening on http://localhost:3000...')// anonimus fuction
+})                                                      
+
+
---
+++ src/students.js
@@ -0,0 +1,13 @@
+let students = ["Michael", "Debbie", "Tre", "Andy", "Diego"]
+
+export function getAllStudents(req, res) {
+    res.send(students)
+}
+
+export function addStudent(req ,res) {
+    const newStudent = req.body
+    students.push(newStudent.name)
+    res.status(201).send({message: "Student added.", success: true})
+}
+
+
